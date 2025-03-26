1928cb078dca7849a9fb806da45ba323fdc5635e
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-03-25 22:36:44 -0300

    Alt

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+
+    // formData - Armaneza os Dados do Formulário, inicialmente vazias
+    // setFormData - Recebe os valores do formData da Properties
+    const [formData, setFormData] = useState({
+        name: '',
+        sobrenome: '',
+        email: '',
+        date: '',
+        password: ''
+    })
+
+    // Função chamada sempre que o Valor de um Campo do Formulário for mudada
+    const handleChange = (e) => {
+        // Pega valor 'name' digitado pelo usúario
+        const { name, value } = e.target
+
+        // Atualiza estado (formData) com o Novo valor do Campo Correspondente
+        setFormData({
+            ...formData, // Mantém os valores anteriores do formData
+            [name]: value // Atualiza o campo específico com o novo valor
+        })
+    }
+
+    const handleSubmit = (e) => {
+        // Evita Carregamento da Página
+        e.preventDefault()
+    }
+
     return (
         <div>
-            <form action="">
-                <label htmlFor="">Nome</label>
-                <input type="text" />
+            <form action="" method='POST'>
+
+                <label htmlFor="name">Nome</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={formData.name} // Valor controlado pelo useSate 'formData'
+                    onChange={handleChange} // Atualiza o useState com o novo valor sempre que o campo mudar
+                />
+
+                <label htmlFor="sobrenome"></label>
+                <input
+                    type="text"
+                    name="sobrenome"
+                    id="sobrenome"
+                    value={formData.sobrenome}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="email">E-mail</label>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="date">Data de Nascimento</label>
+                <input type="date"
+                    name="date"
+                    id="date"
+                    value={formData.date}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="password">Senha</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+
+                <button type='submit'>Cadastrar</button>
             </form>
         </div>
     )
