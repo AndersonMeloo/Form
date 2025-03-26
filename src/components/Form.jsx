c1233aa7495c0ca3de69edfa3e436388c3ac2c1f
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-03-25 21:48:54 -0300

    Alt

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">Nome</label>
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
+
+export default Form
